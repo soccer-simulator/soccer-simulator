3c4a3e413f465cf35bd12d6576465f6646dcf726
Dmitriy Pushkov <ezze@ezze.org>
2024-08-06 22:49:37 +0300

    Prepare boilerplate for CLI

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DataCommand } from './cli/data-command';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DataCommand],
 })
 export class AppModule {}
---
+++ src/cli/data-command.ts
@@ -0,0 +1,9 @@
+import { Command, CommandRunner } from 'nest-commander';
+
+@Command({ name: 'data', description: 'initialize/synchronize data' })
+export class DataCommand extends CommandRunner {
+  async run(): Promise<void> {
+    // TODO: connect to API providing data
+    console.log('Fetch teams & players data here...');
+  }
+}
