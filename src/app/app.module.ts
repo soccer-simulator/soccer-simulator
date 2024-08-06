a2e54bde38b1ce4151a9ddd5d3d64b7edefd3b57
Dmitriy Pushkov <ezze@ezze.org>
2024-08-06 23:29:10 +0300

    Simple docker file for the app is created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DataCommand } from './cli/data-command';
+import { DataCommand } from '../cli/data-command';
 
 @Module({
   imports: [],
--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,6 @@
 import { CommandFactory } from 'nest-commander';
 
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   await CommandFactory.run(AppModule, ['warn', 'error']);
