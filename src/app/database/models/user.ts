3dce74638c0b566a79b0cf88b7b55d43ad759637
Dmitriy Pushkov <ezze@ezze.org>
2024-08-09 00:57:58 +0300

    Use less descriptive User model

--- src/app/database/models/user.ts
+++ src/app/database/models/user.ts
@@ -1,19 +1,7 @@
-import { Optional } from 'sequelize';
 import { Column, Model, PrimaryKey, Table } from 'sequelize-typescript';
 
-interface UserAttributes {
-  createdAt: Date;
-  updatedAt?: Date;
-  id: number;
-  login: string;
-  password: string;
-  admin: boolean;
-}
-
-interface UserCreationAttributes extends Optional<UserAttributes, 'id'> {}
-
 @Table({ modelName: 'user' })
-export class User extends Model<UserAttributes, UserCreationAttributes> {
+export class User extends Model {
   @Column
   createdAt: Date;
 
