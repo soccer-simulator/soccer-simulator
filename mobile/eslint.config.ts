abbbdf5e2610e31b09f2ad774fad2abea4a62f23
Dmitriy Pushkov <ezze@ezze.org>
2024-12-24 22:58:18 +0300

    Add prettier packages

--- mobile/eslint.config.ts
+++ mobile/eslint.config.ts
@@ -1,6 +1,7 @@
 import { Linter } from 'eslint';
+import ezzePrettier from 'eslint-config-ezze-prettier';
 import ezzeTypeScript from 'eslint-config-ezze-ts';
 
-const config: Array<Linter.Config> = [...ezzeTypeScript];
+const config: Array<Linter.Config> = [...ezzeTypeScript, ...ezzePrettier];
 
 export default config;
